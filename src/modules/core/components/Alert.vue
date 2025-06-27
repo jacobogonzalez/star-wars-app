783757878ff878e83c13751adfd49d730d580522
Jacobo Gonzalez <jacobogonzalezdorta@gmail.com>
2025-06-27 08:14:48 +0100

    fix interface alert component

--- src/modules/core/components/Alert.vue
+++ src/modules/core/components/Alert.vue
@@ -5,9 +5,11 @@
 </template>
 
 <script lang="ts" setup>
-  // Define component props using Vue 3's script setup.
-  // This component expects two props:
-  // 1. 'type': A string that determines the visual style of the alert (e.g., 'success', 'info', 'warning', 'error').
-  // 2. 'message': A string that contains the text content to be displayed within the alert.
-  const { type, message } = defineProps<{type: string, message: string}>();
+interface Props {
+  type: 'error' | 'info' | 'success' | 'warning';
+  message: string;
+}
+
+// Destructure the props directly from defineProps
+const { type, message } = defineProps<Props>();
 </script>
