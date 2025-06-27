220c1e608e5a65b6a6318d5bc38d588c37b95c7d
Jacobo Gonzalez <jacobogonzalezdorta@gmail.com>
2025-06-27 12:02:17 +0100

    change vitetest

--- vite.config.ts
+++ vite.config.ts
@@ -9,29 +9,25 @@ const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [
-    vue(),            // Plugin Vue para Vite y Vitest
-    tsconfigPaths(),  // Soporte alias de paths TS
+    vue(),
+    tsconfigPaths(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),  // Alias @ apunta a src
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
-    globals: true,        // Permite usar describe, it, expect globalmente
-    environment: 'jsdom', // Emula navegador para tests
-    setupFiles: './src/tests/setup.ts', // Archivo para setup global
-     server: {
-      deps: {
-        inline: ['vuetify'],
-      },
-    },
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.spec.ts', 'src/**/*.test.ts', 'src/modules/dataTable/**/*.spec.ts'],
+    setupFiles: './src/tests/setup.ts',
     deps: {
-      external: [/\.css$/],      // Ignorar archivos CSS (previene error unknown ext)
+      inline: ['vuetify'],     // <- aquí debe ir inline
+      external: [/\.css$/],
     },
-    // Esto es opcional pero útil para debugging y mejores mensajes:
     watch: false,
     clearMocks: true,
-    css: false, // Deshabilita CSS para evitar errores con importaciones CSS
+    css: false,
   },
 })
