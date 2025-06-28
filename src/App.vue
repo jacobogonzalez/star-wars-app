551a049922b59fe484f04f24d40e8cfd7becf88c
Jacobo Gonzalez <jacobogonzalezdorta@gmail.com>
2025-06-28 09:04:45 +0100

    add coments

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+  <!-- Root component rendering the main application layout -->
   <AppLayout />
 </template>
 
 <script setup lang="ts">
-import AppLayout from './modules/core/components/AppLayout.vue'
+  // Importing the main layout component for the app
+  import AppLayout from './modules/core/components/AppLayout.vue';
 </script>
